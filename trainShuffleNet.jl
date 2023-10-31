72149b1d35c9b1db280ed1ec6041c1ba3d746cb2
RafaelT00 <92472445+RafaelT00@users.noreply.github.com>
2023-10-31 16:10:29 +0100

    Update trainShuffleNet.jl

--- trainShuffleNet.jl
+++ trainShuffleNet.jl
@@ -8,7 +8,7 @@ task = ImageClassificationSingle(blocks, size=(128, 128))
 
 backbone = get_shufflenet(1, 1; in_channels=3, num_classes=10)
 
-learner = tasklearner(task, data; callbacks=[ToGPU(), Metrics(accuracy)], optimizer=Optimisers.Adam())
+learner = tasklearner(task, data; callbacks=[ToGPU(), Metrics(accuracy)], optimizer=Optimisers.Adam(), backbone)
 
 fitonecycle!(learner, 10, 0.004)
 
